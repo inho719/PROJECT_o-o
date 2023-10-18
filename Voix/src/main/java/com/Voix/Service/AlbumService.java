3d449310926a828ebe1cc2bf8a0f82ae5ac3ac2d
inho719 <61335266+inho719@users.noreply.github.com>
2023-10-18 21:14:20 +0900

    Update AlbumService.java
    검색

--- Voix/src/main/java/com/Voix/Service/AlbumService.java
+++ Voix/src/main/java/com/Voix/Service/AlbumService.java
@@ -194,5 +194,21 @@ public class AlbumService {
 		return adao.getAlbumInfo_alcode(caalcode);
 	}
 
-
+public ArrayList<HashMap<String, String>> selectTitle(String searchKeyword) {
+		ArrayList<HashMap<String, String>> searchList = new ArrayList<HashMap<String, String>>();
+		ArrayList<HashMap<String, String>> titleList = new ArrayList<HashMap<String, String>>();
+		ArrayList<HashMap<String, String>> contentList = new ArrayList<HashMap<String, String>>();
+			try {
+				titleList = adao.selectSearch_Title(searchKeyword);				
+			} catch (Exception e) {
+			}
+			System.out.println(titleList);
+			try {
+				contentList = adao.selectSearch_Content(searchKeyword);				
+			} catch (Exception e) {
+			}
+			searchList.addAll(titleList);
+			searchList.addAll(contentList);
+		return searchList;
+	} 
 }
