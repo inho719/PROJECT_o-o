435ea0cfa0a51d7b79defa74a8f64f2a37d684a4
inho719 <61335266+inho719@users.noreply.github.com>
2023-10-18 21:13:44 +0900

    Update HomeController.java
    검색

--- Voix/src/main/java/com/Voix/Controller/HomeController.java
+++ Voix/src/main/java/com/Voix/Controller/HomeController.java
@@ -15,6 +15,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+
+@Autowired
+	private AlbumService asvc;
+	
+	@Autowired
+	private ChartService csvc;
+	
+	@Autowired
+	private NewsService nsvc;
+	
+	@Autowired
+	private TicketService tsvc;
+
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model,HttpSession session) {
 		System.out.println("메인페이지 이동");
@@ -24,6 +38,37 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		session.setAttribute("sideState", "M");
 		return "MainPage";
+	}
+
+	@RequestMapping("/getSearch")
+	public ModelAndView search(String searchKeyword, String pageType) {
+		ModelAndView mav= new ModelAndView();
+		
+		switch(pageType) {
+		case "AlbumPage":
+			ArrayList<HashMap<String, String>> AlbumList = asvc.selectTitle(searchKeyword);
+			mav.addObject("AlbumListMap",AlbumList);
+			mav.setViewName("Basic/"+pageType);
+			break;
+		case "ChartPage":
+			ArrayList<HashMap<String, String>> ChartList = csvc.selectTitle(searchKeyword);
+			mav.addObject("ChartListMap",ChartList);
+			mav.setViewName("Basic/"+pageType);
+			break;
+		case "NewsPage":
+			ArrayList<HashMap<String, String>> NewsList = nsvc.selectTitle(searchKeyword);
+			mav.addObject("NewsListMap",NewsList);
+			mav.setViewName("Basic/"+pageType);
+			break;
+		case "TicketPage":
+			ArrayList<HashMap<String, String>> TicketList = tsvc.selectTitle(searchKeyword);
+			mav.addObject("TkListMap",TicketList);
+			mav.setViewName("Basic/"+pageType);
+			break;
+		}
+		
+		
+		return mav;
 	}
 	
 }
