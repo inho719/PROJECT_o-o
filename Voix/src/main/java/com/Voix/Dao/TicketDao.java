9659635cf828c09cd243e9f807edb7d77267265a
inho719 <61335266+inho719@users.noreply.github.com>
2023-10-18 21:16:24 +0900

    Update TicketDao.java

--- Voix/src/main/java/com/Voix/Dao/TicketDao.java
+++ Voix/src/main/java/com/Voix/Dao/TicketDao.java
@@ -23,5 +23,9 @@ public interface TicketDao {
 	
 	ArrayList<Ticket> getTkTitle(String tkplace);
 
+	ArrayList<HashMap<String, String>> selectSearch_Title(String searchKeyword);
+
+	ArrayList<HashMap<String, String>> selectSearch_Content(String searchKeyword);
+
 
 }
