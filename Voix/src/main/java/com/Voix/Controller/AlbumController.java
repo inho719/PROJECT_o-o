855fedbd856a43af6020b483f218972180f80508
JIHEON HAN <64591492+JIheonHan1123@users.noreply.github.com>
2023-10-20 19:13:09 +0900

    Update AlbumController.java
    AlbumInfoPage 수정
    getYes24Pop 생성
    getYes24Kpop 생성

--- Voix/src/main/java/com/Voix/Controller/AlbumController.java
+++ Voix/src/main/java/com/Voix/Controller/AlbumController.java
@@ -1,11 +1,13 @@
 package com.Voix.Controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +17,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.Voix.Dto.Album;
-import com.Voix.Dto.Order;
 import com.Voix.Service.AlbumService;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -42,15 +43,16 @@ public class AlbumController {
 	}
 
 	@RequestMapping(value = "/AlbumInfoPage")
-	public ModelAndView AlbumInfoPage(String altitle) {
+	public ModelAndView NewsInfoPage(String alcode) {
 		ModelAndView mav = new ModelAndView();
-		ArrayList<Album> AlbumInfoList = asvc.getAlbumInfoList(altitle);
+		ArrayList<Album> AlbumInfoList = asvc.getAlbumInfoList(alcode);
 		Album ALInfo = AlbumInfoList.get(0);
 		System.out.println(AlbumInfoList);
 		mav.addObject("ALInfo", ALInfo);
 		mav.addObject("AlbumInfoList", AlbumInfoList);
-		ArrayList<HashMap<String,String>> reviewList = asvc.selectReviewList(altitle);
-		mav.addObject("reviewList",reviewList);
+
+		ArrayList<HashMap<String, String>> reviewList = asvc.selectReviewList(alcode);
+		mav.addObject("reviewList", reviewList);
 
 		mav.setViewName("BasicInfo/AlbumInfoPage");
 		return mav;
@@ -58,22 +60,22 @@ public class AlbumController {
 
 	@RequestMapping(value = "/albumRegistReview")
 	public ModelAndView registReview(String restate, String recontent, HttpSession session, RedirectAttributes ra) {
-		String rewriter = (String)session.getAttribute("loginId");
+		String rewriter = (String) session.getAttribute("loginId");
 		int registResult = asvc.albumRegistReview(restate, recontent, rewriter);
 		ModelAndView mav = new ModelAndView();
 		ra.addFlashAttribute("msg", "댓글이 등록 되었습니다.");
-		mav.setViewName("redirect:/AlbumInfoPage?altitle="+restate);
+		mav.setViewName("redirect:/AlbumInfoPage?altitle=" + restate);
 		return mav;
-		
+
 	}
-	
-	@RequestMapping(value="/albumDeleteReview")
-	public ModelAndView deleteReivew(String recode,String altitle,RedirectAttributes ra) {
+
+	@RequestMapping(value = "/albumDeleteReview")
+	public ModelAndView deleteReivew(String recode, String altitle, RedirectAttributes ra) {
 		System.out.println("리뷰 삭제 요청");
 		ModelAndView mav = new ModelAndView();
 		int Result = asvc.deleteReview(recode);
 		ra.addFlashAttribute("msg", "댓글 삭제 완료 되었습니다.");
-		mav.setViewName("redirect:/AlbumInfoPage?altitle="+altitle);
+		mav.setViewName("redirect:/AlbumInfoPage?altitle=" + altitle);
 
 		return mav;
 	}
@@ -82,12 +84,13 @@ public class AlbumController {
 	public ModelAndView CartPage(HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		String loginId = (String) session.getAttribute("loginId");
-				ArrayList<HashMap<String,String>> CartList = asvc.CartList(loginId);
-				System.out.println(CartList);
-				mav.addObject("CartList", CartList);
-				mav.setViewName("Basic/CartPage");
+		ArrayList<HashMap<String, String>> CartList = asvc.CartList(loginId);
+		System.out.println(CartList);
+		mav.addObject("CartList", CartList);
+		mav.setViewName("Basic/CartPage");
 		return mav;
 	}
+
 	@RequestMapping(value = "/InsertCartPage")
 	public ModelAndView InsertCartPage(String caalcode, int caqty, HttpSession session, RedirectAttributes ra) {
 		ModelAndView mav = new ModelAndView();
@@ -105,69 +108,70 @@ public class AlbumController {
 			if (insertCart > 0) {
 				System.out.println("장바구니 추가 완료");
 				mav.setViewName("redirect:/CartPage");
-			}else {
+			} else {
 				System.out.println("장바구니 추가 실패");
-				ra.addFlashAttribute("msg","항목을 장바구니에 추가하는데 실패하였습니다. 다시 시도해주세요");
+				ra.addFlashAttribute("msg", "항목을 장바구니에 추가하는데 실패하였습니다. 다시 시도해주세요");
 				mav.setViewName("redirect:/AlbumInfoPage");
 			}
 		}
 		return mav;
 	}
+
 	@RequestMapping(value = "/DeleteCartList", method = RequestMethod.POST)
 	public ModelAndView DeleteCartList(@RequestParam(value = "CartCheck") String[] CartCheck) {
 		ModelAndView mav = new ModelAndView();
-		
-		for(int i = 0; i < CartCheck.length; i++) {
+
+		for (int i = 0; i < CartCheck.length; i++) {
 			System.out.println(CartCheck[i]);
 			asvc.DeleteCartList(CartCheck[i]);
 		}
-		
+
 		mav.setViewName("redirect:/CartPage");
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/PayPage")
-	public ModelAndView PayPage(String[] selAl,HttpSession session,RedirectAttributes ra) {
+	public ModelAndView PayPage(String[] selAl, HttpSession session, RedirectAttributes ra) {
 		ModelAndView mav = new ModelAndView();
 		String mid = (String) session.getAttribute("loginId");
 		if (mid == null) {
 			System.out.println("로그인 안함");
 			ra.addFlashAttribute("msg", "결제는 로그인후 이용 가능합니다.");
-			
+
 			mav.setViewName("redirect:/LoginPage");
-			
+
 		} else if (mid != null) {
 			System.out.println("로그인  함");
 			ArrayList<Album> alInfoList = new ArrayList<Album>();
-			for(String al : selAl) {
+			for (String al : selAl) {
 				String[] al_Spilt = al.split("_");
 				String alcode = al_Spilt[0];
 				String alqty = al_Spilt[1];
-				
-				if(al_Spilt.length > 2) {
+
+				if (al_Spilt.length > 2) {
 					String cacode = null;
 					cacode = al.split("_")[2];
 					int deleteResult = asvc.DeleteCartList(cacode);
 					System.out.println(deleteResult);
 				}
 				Album alInfo = asvc.getAlbumInfo_alcode(alcode);
-				alInfo.setAlqty( Integer.parseInt(alqty)  );
+				alInfo.setAlqty(Integer.parseInt(alqty));
 				alInfoList.add(alInfo);
 			}
 			System.out.println("결제페이지이동 완료");
-			
-			mav.addObject("mid",mid);
-			mav.addObject("alList",alInfoList);
+
+			mav.addObject("mid", mid);
+			mav.addObject("alList", alInfoList);
 			mav.setViewName("Basic/PayPage");
 		}
 		return mav;
 	}
-	
-	@RequestMapping(value="/kakaoPay_ready")
-	public @ResponseBody String kakaoPay_ready(String odmid,String odaddr,String orList,HttpSession session) {
+
+	@RequestMapping(value = "/kakaoPay_ready")
+	public @ResponseBody String kakaoPay_ready(String odmid, String odaddr, String orList, HttpSession session) {
 		System.out.println("카카오 결제 준비 요청 - / kakaoPay_ready");
-		//odInfo.setOdcode(odcode);
-		
+		// odInfo.setOdcode(odcode);
+
 		Gson gson = new Gson();
 		JsonArray orList_arr = JsonParser.parseString(orList).getAsJsonArray();
 		Order odInfo = new Order();
@@ -177,26 +181,26 @@ public class AlbumController {
 		int totalQty = 0;
 		String[] codeList = new String[orList_arr.size()];
 		int i = 0;
-		for( JsonElement od :  orList_arr ) {
+		for (JsonElement od : orList_arr) {
 			Order oddto = gson.fromJson(od, Order.class);
 			oddto.setOdcode(asvc.odcodeseting());
 			oddto.setOdaddr(odaddr);
 			oddto.setOdmid(odmid);
 			System.out.println(oddto);
-			totalPrice +=  od.getAsJsonObject().get("odprice").getAsInt();
-			totalQty +=  od.getAsJsonObject().get("odqty").getAsInt(); 
-			codeList[i]=oddto.getOdcode();
+			totalPrice += od.getAsJsonObject().get("odprice").getAsInt();
+			totalQty += od.getAsJsonObject().get("odqty").getAsInt();
+			codeList[i] = oddto.getOdcode();
 			i++;
-		
+
 			int insertOd = asvc.insertOdInfo(oddto);
 		}
-		for(String testStr : codeList) {
+		for (String testStr : codeList) {
 			System.out.println("code : " + testStr);
 		}
-		session.setAttribute("codeList",codeList );
-		odInfo.setOdprice(totalPrice+"");
-		odInfo.setOdqty(totalQty+"");
-		
+		session.setAttribute("codeList", codeList);
+		odInfo.setOdprice(totalPrice + "");
+		odInfo.setOdqty(totalQty + "");
+
 		String result = asvc.kakaoPay_ready(odInfo, session);
 		return result;
 	}
@@ -206,37 +210,37 @@ public class AlbumController {
 		System.out.println("카카오 결제 승인 요청");
 		ModelAndView mav = new ModelAndView();
 		System.out.println("pg_token : " + pg_token);
-		String tid = (String)session.getAttribute("tid");
+		String tid = (String) session.getAttribute("tid");
 		System.out.println("tid : " + tid);
 		ArrayList<String> odcodeList = new ArrayList<String>();
-		session.setAttribute("odcodeList",odcodeList);
+		session.setAttribute("odcodeList", odcodeList);
 		System.out.println(session.getAttribute("odcodeList"));
-		String odcode = (String)session.getAttribute("odcode");
-		String loginId = (String)session.getAttribute("loginId");
-		String result = asvc.kakaoPay_approval(tid, pg_token, odcode, loginId ); 
-		if( result == null) {
-		System.out.println("결제 오류");
-		String[] codeList = (String[]) session.getAttribute("codeList");
-		for(int i =0; i < codeList.length; i++) {
-			System.out.println(codeList[i]);
-			asvc.cancelReserve(codeList[i]);
-		}
-		mav.addObject("payResult", "N");
+		String odcode = (String) session.getAttribute("odcode");
+		String loginId = (String) session.getAttribute("loginId");
+		String result = asvc.kakaoPay_approval(tid, pg_token, odcode, loginId);
+		if (result == null) {
+			System.out.println("결제 오류");
+			String[] codeList = (String[]) session.getAttribute("codeList");
+			for (int i = 0; i < codeList.length; i++) {
+				System.out.println(codeList[i]);
+				asvc.cancelReserve(codeList[i]);
+			}
+			mav.addObject("payResult", "N");
 		} else {
-		System.out.println("결제 승인");
-		mav.addObject("payResult", "Y");
+			System.out.println("결제 승인");
+			mav.addObject("payResult", "Y");
 		}
 		session.removeAttribute("mid");
 		session.removeAttribute("odcode");
 		mav.setViewName("/Basic/PayResult");
 		return mav;
-		}
+	}
 
-		@RequestMapping(value = "/kakaoPay_cancel")
-		public ModelAndView kakaoPay_cancel(HttpSession session) {
+	@RequestMapping(value = "/kakaoPay_cancel")
+	public ModelAndView kakaoPay_cancel(HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		String[] codeList = (String[]) session.getAttribute("codeList");
-		for(int i =0; i < codeList.length; i++) {
+		for (int i = 0; i < codeList.length; i++) {
 			System.out.println(codeList[i]);
 			asvc.cancelReserve(codeList[i]);
 		}
@@ -244,9 +248,36 @@ public class AlbumController {
 		mav.addObject("payResult", "N");
 		mav.setViewName("/Basic/PayResult");
 		return mav;
-		}
-	
-	
-	
+	}
+
+	// 팝송 앨범 크롤링
+	@RequestMapping(value = "/getYes24Pop")
+	public ModelAndView getYes24Pop() throws IOException {
+		System.out.println("Yes24 앨범(팝송) 정보 수집 요청");
+
+		// 추가된 앨범 개수 받아올 거
+		int addCount = asvc.addYes24Pop();
+		System.out.println("추가: " + addCount);
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:/");
+
+		return mav;
+	}
+
+	// 가요 앨범 크롤링
+	@RequestMapping(value = "/getYes24Kpop")
+	public ModelAndView getYes24Kpop() throws IOException {
+		System.out.println("Yes24 앨범(가요) 정보 수집 요청");
+
+		// 추가된 앨범 개수 받아올 거
+		int addCount = asvc.addYes24Kpop();
+		System.out.println("추가: " + addCount);
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:/");
+
+		return mav;
+	}
 
 }
