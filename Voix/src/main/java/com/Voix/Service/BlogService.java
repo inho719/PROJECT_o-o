9f2349ade325f36f0aa9cef9d1df19c140091326
HappyKity <145517307+HappyKity@users.noreply.github.com>
2023-10-24 19:00:28 +0900

    Add files via upload
    블로그 기능 활성화

--- Voix/src/main/java/com/Voix/Service/BlogService.java
+++ Voix/src/main/java/com/Voix/Service/BlogService.java
@@ -1,11 +1,13 @@
 package com.Voix.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Voix.Dao.BlogDao;
+import com.Voix.Dto.Blog;
 
 @Service
 public class BlogService {
@@ -13,9 +15,58 @@ public class BlogService {
 	@Autowired
 	private BlogDao bdao;
 	
+	@Autowired
+	private TicketService tsvc;
 	
 	public HashMap<String, String> selectMainBlog(String themaStr) {
 		return bdao.selectMainBlog();
+	}
+
+
+	public ArrayList<HashMap<String, String>> getBlogList_map() {
+
+		return bdao.selectBlog_map();
+	}
+
+
+	public Blog getbgInfo(String bgcode) {
+		System.out.println("SERVICE - 블로그정보 출력");
+		return bdao.getBlogInfo(bgcode);
+	}
+
+
+	public ArrayList<HashMap<String, String>> selectReviewList(String bgcode) {
+		System.out.println("selectReivewList  호출");
+		ArrayList<HashMap<String, String>> Resultre = bdao.selectReviewList(bgcode);
+		return Resultre;
+	}
+
+
+	public int BlogRegistReview(String restate, String recontent, String rewriter) {
+		System.out.println("service - registReview()");
+		String maxRvCode = bdao.selectMaxReCode();
+		String recode = tsvc.genCode(maxRvCode);
+		int registReview = bdao.BlogRegistReview(recode, restate, recontent, rewriter);
+		System.out.println(registReview);
+		return registReview;
+	}
+
+
+	public int deleteBlogReview(String recode) {
+		System.out.println("SERVEICE - deleteReview 호출");
+		return bdao.deleteBlogReview(recode);
+	}
+
+
+	public int likeBlog(String like, String mid) {
+		System.out.println("SERVICE- 블로그 찜");
+		return bdao.likeBlog(like,mid);
+	}
+
+
+	public ArrayList<HashMap<String, String>> selectBlogHitList() {
+		System.out.println("조회수 목록 조회");
+		return bdao.selectBlogHitList();
 	}
 
 }
