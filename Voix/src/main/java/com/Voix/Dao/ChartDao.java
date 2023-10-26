4c7fd0183152adfcf9e2c402e26411a0552bb599
kimstandard <145517265+kimstandard@users.noreply.github.com>
2023-10-26 19:40:55 +0900

    Update ChartDao.java
    페이징추가

--- Voix/src/main/java/com/Voix/Dao/ChartDao.java
+++ Voix/src/main/java/com/Voix/Dao/ChartDao.java
@@ -21,4 +21,8 @@ public interface ChartDao {
 	String selectMaxSgCode();
 
 	int insertSong(Song song);
+
+	int countBoard();
+	
+	List<Map<String, Object>> selectBoardList(@Param("pageStart") String startSGCODE, @Param("perPageNum")String endSGCODE);
 }
