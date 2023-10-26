a1683f0481b5fa179d486daee8b8a52400aea28d
JIHEON HAN <64591492+JIheonHan1123@users.noreply.github.com>
2023-10-26 18:00:45 +0900

    Update NewsService.java

--- Voix/src/main/java/com/Voix/Service/NewsService.java
+++ Voix/src/main/java/com/Voix/Service/NewsService.java
@@ -3,6 +3,8 @@ package com.Voix.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
