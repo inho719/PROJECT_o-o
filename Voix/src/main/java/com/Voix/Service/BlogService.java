1763181befafc8240b379c8d6279fb1cbc81e284
inho719 <61335266+inho719@users.noreply.github.com>
2023-10-26 20:38:53 +0900

    Update BlogService.java

--- Voix/src/main/java/com/Voix/Service/BlogService.java
+++ Voix/src/main/java/com/Voix/Service/BlogService.java
@@ -97,6 +97,141 @@ public class BlogService {
 		System.out.println("SERVICE - 페이징 넘길 때 코드");
 		return bdao.selectBoardList(startBGCODE, endBGCODE);
 	}
+public String chatbotAPI(String inputText,String mid) {
+		String secretKey = "cUlaRmRJSlNVYWVXY0NMb2tFdUtxUldEVWdRa1pPRU4=";
+		String apiURL = "https://7mfycmpb8n.apigw.ntruss.com/VOIX/VOIX/";
 
+		String chatbotMessage = ""; // 응답 메세지
+		try {
+			// String apiURL = "https://ex9av8bv0e.apigw.ntruss.com/custom_chatbot/prod/";
+
+			URL url = new URL(apiURL);
+
+			String message = getReqMessage(inputText);
+			System.out.println("##" + message);
+
+			String encodeBase64String = makeSignature(message, secretKey);
+
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json;UTF-8");
+			con.setRequestProperty("X-NCP-CHATBOT_SIGNATURE", encodeBase64String);
+
+			// post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.write(message.getBytes("UTF-8"));
+			wr.flush();
+			wr.close();
+			int responseCode = con.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+			BufferedReader br;
+
+			if (responseCode == 200) { // Normal call
+				System.out.println(con.getResponseMessage());
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = in.readLine()) != null) {
+					sb.append(line);
+				}
+				in.close();
+				con.disconnect();
+				System.out.println(sb.toString());
+				String data1 = JsonParser.parseString(sb.toString())
+						.getAsJsonObject().get("bubbles").getAsJsonArray().get(0).getAsJsonObject().get("data").getAsJsonObject().get("description").getAsString();
+				System.out.println(data1);
+				chatbotMessage = data1;
+			} else { // Error occurred
+				chatbotMessage = con.getResponseMessage();
+			}
+		} catch (Exception e) {
+			System.out.println("ex"+e);
+		}
+		return chatbotMessage;
+	}
+
+	public static String makeSignature(String message, String secretKey) {
+
+		String encodeBase64String = "";
+
+		try {
+			byte[] secrete_key_bytes = secretKey.getBytes("UTF-8");
+
+			SecretKeySpec signingKey = new SecretKeySpec(secrete_key_bytes, "HmacSHA256");
+			Mac mac = Mac.getInstance("HmacSHA256");
+			mac.init(signingKey);
+
+			byte[] rawHmac = mac.doFinal(message.getBytes("UTF-8"));
+			encodeBase64String = Base64.getEncoder().encodeToString(rawHmac);
+			return encodeBase64String;
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return encodeBase64String;
+
+	}
+
+	public static String getReqMessage(String voiceMessage) {
+
+		String requestBody = "";
+
+		try {
+
+			JsonObject obj_gson = new JsonObject();
+
+			long timestamp = new Date().getTime();
+
+			System.out.println("##" + timestamp);
+			obj_gson.addProperty("version", "v2");
+			obj_gson.addProperty("userId", "U47b00b58c90f8e47428af8b7bddc1231heo2");
+//=> userId is a unique code for each chat user, not a fixed value, recommend use UUID. use different id for each user could help you to split chat history for users.
+			obj_gson.addProperty("timestamp", timestamp);
+
+			JsonObject bubbles_obj_gson = new JsonObject();
+
+			JsonObject data_obj_gson = new JsonObject();
+			data_obj_gson.addProperty("description", voiceMessage);
+
+			bubbles_obj_gson.addProperty("type", "text");
+			bubbles_obj_gson.add("data", data_obj_gson);
+
+			JsonArray bubbles_array_gson = new JsonArray();
+			bubbles_array_gson.add(bubbles_obj_gson);
+
+			obj_gson.add("bubbles", bubbles_array_gson);
+
+			obj_gson.addProperty("event", "send");
+
+			requestBody = new Gson().toJson(obj_gson);
+
+		} catch (Exception e) {
+			System.out.println("## Exception : " + e);
+		}
+		return requestBody;
+	}
+
+	public String jsonToString(String jsonResultStr) {
+		String resultText = "";
+		// API 호출 결과 받은 JSON 형태 문자열에서 텍스트 추출
+		// JSONParser 사용하지 않음
+
+		JsonObject jsonObj_gson = JsonParser.parseString(jsonResultStr).getAsJsonObject();
+		JsonArray chatArray_gson = jsonObj_gson.get("bubbles").getAsJsonArray();
+		System.out.println("chatArray_gson.size : " + chatArray_gson.size());
+		
+		if (chatArray_gson.size() > 0) {
+			JsonObject tempObj_gson = chatArray_gson.get(0).getAsJsonObject();
+			JsonObject dataObj_gson = tempObj_gson.get("data").getAsJsonObject();
+			if (dataObj_gson != null) {
+				resultText += dataObj_gson.get("description").getAsString();
+			}
+		} else {
+			System.out.println("없음");
+		}
+		return resultText;
+	}
 	
 }
